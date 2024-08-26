8dd58f43df630343beede8576c67214d399a00b1
Kubachoo <koichiseto61@gmail.com>
2024-08-26 14:49:29 -0400

    Added final report function to code

--- main.py
+++ main.py
@@ -2,7 +2,11 @@ def main():
     book_path = "books/frankenstein.txt"
     text = get_book_text(book_path)
     num_words = get_num_words(text)
-    print(f"{num_words} words found in the document")
+    print(f"--- Begin report of {book_path} ---")
+    print(f"{num_words} words found in the document\n")
+    num_characters = count_characters(text)
+    getReport = return_report(count_characters(text))
+    print("--- End report ---")
 
 
 def get_num_words(text):
@@ -13,6 +17,22 @@ def get_num_words(text):
 def get_book_text(path):
     with open(path) as f:
         return f.read()
+
+def count_characters(words):
+    charNums = {}
+    for char in words.lower():
+       charNums[char] = charNums.get(char, 0) + 1
+    return charNums
+
+def return_report(dictionary):
+    char_list = list(dictionary.items())
+    sorted_chars = sorted(char_list, key=lambda x:x[1], reverse=True)
+
+    for char_tuple in sorted_chars:
+        char = char_tuple[0]
+        count = char_tuple[1]
+        if char.isalpha():
+            print(f"The '{char}' character was found {count} times")
 
 
 main()
